30651f9221a85570f07d30a41d3d142de87a63f1
Ayazhan <ayazhan1508@gmail.com>
2019-05-21 12:05:32 +0300

    Update frame.hpp

--- frame.hpp
+++ frame.hpp
@@ -20,50 +20,50 @@ public:
 
 class MainFrame : public wxFrame {
 public:
-    explicit MainFrame(const wxString& title, const wxSize& size);
+	explicit MainFrame(const wxString& title, const wxSize& size);
 
-    void OnZero (wxCommandEvent&);
-    void OnOne  (wxCommandEvent&);
-    void OnTwo  (wxCommandEvent&);
-    void OnThree(wxCommandEvent&);
-    void OnFour (wxCommandEvent&);
-    void OnFive (wxCommandEvent&);
-    void OnSix  (wxCommandEvent&);
-    void OnSeven(wxCommandEvent&);
-    void OnEight(wxCommandEvent&);
-    void OnNine (wxCommandEvent&);
+	void OnZero (wxCommandEvent&);
+	void OnOne  (wxCommandEvent&);
+	void OnTwo  (wxCommandEvent&);
+	void OnThree(wxCommandEvent&);
+	void OnFour (wxCommandEvent&);
+	void OnFive (wxCommandEvent&);
+	void OnSix  (wxCommandEvent&);
+	void OnSeven(wxCommandEvent&);
+	void OnEight(wxCommandEvent&);
+	void OnNine (wxCommandEvent&);
 
-    void OnAddOrSin     (wxCommandEvent&);
-    void OnSubtractOrCos(wxCommandEvent&);
-    void OnMultiplyOrPi (wxCommandEvent&);
-    void OnDivideOrExp  (wxCommandEvent&);
-    void OnInsert       (wxCommandEvent&);
-    void OnSwapOrLog    (wxCommandEvent&);
+	void OnAddOrSin     (wxCommandEvent&);
+	void OnSubtractOrCos(wxCommandEvent&);
+	void OnMultiplyOrPi (wxCommandEvent&);
+	void OnDivideOrExp  (wxCommandEvent&);
+	void OnInsert       (wxCommandEvent&);
+	void OnSwapOrLog    (wxCommandEvent&);
 
-    void OnPointOrInverseOrScrollLeft   (wxCommandEvent&);
-    void OnNegativeOrSquareOrScrollRight(wxCommandEvent&);
-    void OnScientificNotationOrSqrt     (wxCommandEvent&);
-    void OnF                            (wxCommandEvent&);
+	void OnPointOrInverseOrScrollLeft   (wxCommandEvent&);
+	void OnNegativeOrSquareOrScrollRight(wxCommandEvent&);
+	void OnScientificNotationOrSqrt     (wxCommandEvent&);
+	void OnF                            (wxCommandEvent&);
 
-    void OnP    (wxCommandEvent&);
-    void OnPower(wxCommandEvent&);
-    void OnShg  (wxCommandEvent&);
-    void OnShg2 (wxCommandEvent&);
-    void OnVo   (wxCommandEvent&);
-    void OnSp   (wxCommandEvent&);
-    void OnBp   (wxCommandEvent&);
-    void OnPp   (wxCommandEvent&);
-    void OnCx   (wxCommandEvent&);
+	void OnP    (wxCommandEvent&);
+	void OnPower(wxCommandEvent&);
+	void OnShg  (wxCommandEvent&);
+	void OnShg2 (wxCommandEvent&);
+	void OnVo   (wxCommandEvent&);
+	void OnSp   (wxCommandEvent&);
+	void OnBp   (wxCommandEvent&);
+	void OnPp   (wxCommandEvent&);
+	void OnCx   (wxCommandEvent&);
 
 
 private:
-    const wxSize BUTTON_SIZE = {40, 40};
-    static size_t MakeId (Button button) { return static_cast<size_t>(button); }
+	const wxSize BUTTON_SIZE = {40, 40};
+	static size_t MakeId (Button button) { return static_cast<size_t>(button); }
 
-    // Принимает либо метку на простую операцию, либо на операцию с записываемым текстом
-	void UpdateTextCtrl(Calculator::TextCtrlModifier&& item);
+	// Принимает либо метку на простую операцию, либо на операцию с записываемым текстом
+	void UpdateTextCtrl(Calculator::TextCtrlModifier item);
 
-    wxPanel*    panel     ;
-    wxTextCtrl* text_ctrl ;
-    Calculator  calculator;
+	wxPanel*    panel     ;
+	wxTextCtrl* text_ctrl ;
+	Calculator  calculator;
 };
